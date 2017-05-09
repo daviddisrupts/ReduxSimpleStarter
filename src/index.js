b3cf4e36e9137dc8ed2e056b55e82428f34655ad
David Brooks <davidbrooks.now@gmail.com>
2017-05-09 15:01:43 -0700

    props, builiding lists with map, list item keys

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCGxYFSh7g3m_o6iet2iaQJmJfxeM33wV8';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+		this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {this.setState({ videos });
+});
+}
+
+render() {
+	return (
+		<div>
+			<SearchBar />
+			<VideoList videos={this.state.videos} />
+		</div>
+);
+}
+}
+// Take this component's generated HTML and put it on the page [in the DOM]
+
+ReactDOM.render(<App />, document.querySelector('.container'));
