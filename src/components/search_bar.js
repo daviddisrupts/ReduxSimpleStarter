eb0b1105b6e3a6686376b777172d81f6968d8b0d
David Brooks <davidbrooks.now@gmail.com>
2017-05-10 11:59:47 -0700

    added search for videos and throttling search term input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
-// const SearchBar = () => {
-//   return <input />;
-// };
-
-//from functional component to class component
-
 class SearchBar extends Component {
     constructor(props){
       super(props);
 
       this.state = { term: '' };
     }
+
     render() {
       return (
-        <div>
+        <div className="search-bar">
          <input
          value={this.state.term}
-          onChange = {event => this.setState({ term: event.target.value})} />
+          onChange={event => this.onInputChange(event.target.value)} />
          </div>
        );
+    }
+
+    onInputChange(term) {
+      this.setState({term});
+      this.props.onSearchTermChange(term);
     }
 }
 
